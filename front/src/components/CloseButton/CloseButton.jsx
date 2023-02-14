76b5fc65d8e3d417b872a4d8a84eec320900f301
Ekaterina Marinicheva <marri.nich@yandex.ru>
2023-02-14 09:14:09 +0300

    feat: add onClick listener

--- front/src/components/CloseButton/CloseButton.jsx
+++ front/src/components/CloseButton/CloseButton.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './CloseButton.module.scss';
 
-const CloseButton = () => (
-  <button className={styles.close} type="button" aria-label="Close button" />
+const CloseButton = ({ onClose }) => (
+  <button
+    className={styles.close}
+    type="button"
+    aria-label="Close button"
+    onClick={onClose}
+  />
 );
 
 export default CloseButton;

c76efc884b96bc327e12bd195ac2a9fa56cd70eb
Ekaterina Marinicheva <marri.nich@yandex.ru>
2023-02-21 20:06:13 +0300

    refactor: all images in one folder

--- front/src/components/AddRecipe/AddRecipe.jsx
+++ front/src/components/AddRecipe/AddRecipe.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Style from './AddRecipe.module.scss';
 import PageTitle from '../PageTitle/PageTitle';
+import CookingSteps from '../CookingSteps/CookingSteps';
 
 const AddRecipe = () => (
   <main className={Style.content}>
     <PageTitle>Добавить новый рецепт</PageTitle>
+    <CookingSteps />
   </main>
 );
 

75af35287991f6e3cdd95fd64d18f65d13d97054
Ekaterina Marinicheva <marri.nich@yandex.ru>
2023-02-21 18:22:13 +0300

    feat: add cookingSteps

---
+++ front/src/components/CookingSteps/CookingSteps.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import CookingStep from '../CookingStep/CookingStep';
+import Button from '../Button/Button';
+
+import { buttons } from '../../utils/constants';
+
+import styles from './CookingSteps.module.scss';
+import addIcon from '../../img/add.svg';
+
+const CookingSteps = () => {
+  const [initialStepsCount, setInitialStepsCount] = useState(2);
+
+  const handleClickAddStepBtn = () => {
+    setInitialStepsCount((state) => state + 1);
+  };
+
+  return (
+    <div className={styles.steps}>
+      <ul className={styles.steps__list}>
+        {[...Array(initialStepsCount)].map((_, i) => (
+          <li className={styles.steps__item}>
+            <CookingStep stepNumber={i + 1} />
+          </li>
+        ))}
+      </ul>
+      <Button
+        btnClassName={buttons.withBorder.grey}
+        ariaLabelText="Добавить еще один шаг"
+        onClickBtn={handleClickAddStepBtn}
+      >
+        <img src={addIcon} alt="Плюсик" />
+        Добавить шаг
+      </Button>
+    </div>
+  );
+};
+
+export default CookingSteps;

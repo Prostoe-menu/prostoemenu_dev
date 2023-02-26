fd0828b6300820353b01c13cfca3f193611e948c
Hromz <hromizz@gmail.com>
2023-02-26 13:24:57 +0300

    corrected

--- back/recipe/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Recipe
-
-
-class RecipeListView(ListView):
-    model = Recipe
-    template_name = 'recipe_list.html'

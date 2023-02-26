5f56413ff19e7ae9f486c61b397a5e4f1847e7ea
Hromz <hromizz@gmail.com>
2023-02-26 13:43:11 +0300

    commit

---
+++ back/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('recipes/', RecipeList.as_view()),
+    path('recipe/<int:id>/', RecipeDetail.as_view()),
+    path('ingredient/<int:id>/', IngredientDetail.as_view()),
+
+]
+
---
+++ back/api/views.py
@@ -0,0 +1,61 @@
+from rest_framework import status
+from rest_framework.decorators import APIView
+from rest_framework.generics import get_object_or_404
+from rest_framework.settings import api_settings
+from recipe.models import Recipe, Ingredient
+from .serializers import RecipeDisplaySerializer, RecipeCreateSerializer, IngredientSerializer
+from rest_framework.response import Response
+
+
+class RecipeList(APIView):
+    def get(self, request, format=None):
+        recipes = Recipe.objects.all()
+
+        pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
+        paginator = pagination_class()
+
+        page = paginator.paginate_queryset(recipes, request, view=self)
+        serializer = RecipeDisplaySerializer(page, many=True)
+
+        return paginator.get_paginated_response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = RecipeCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            saved_obj = serializer.save()
+            response_data = RecipeDisplaySerializer(saved_obj).data
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        else:
+            default_errors = serializer.errors
+            new_error = {}
+            for field_name, field_errors in default_errors.items():
+                new_error[field_name] = field_errors[0]
+            return Response(new_error, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RecipeDetail(APIView):
+    def get(self, request, id=None):
+        recipe_obj = get_object_or_404(Recipe, id=id)
+
+        if recipe_obj:
+            recipe = Recipe.objects.filter(id=id)
+            serializer = RecipeDisplaySerializer(recipe, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, id=None):
+        recipe = Recipe.objects.filter(id=id)
+        recipe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class IngredientDetail(APIView):
+    def get(self, request, id=None):
+        ingredient_obj = get_object_or_404(Ingredient, id=id)
+        if ingredient_obj:
+            ingredient = Ingredient.objects.get(id=id)
+            serializer = IngredientSerializer(ingredient)
+            return Response(serializer.data, status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)

63316c56f9a58d8949d72c5a3f3135c2cf2a3153
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-03-09 18:09:13 +0100

    ingredients container and button layout is ready

--- front/src/components/AddRecipe/AddRecipe.jsx
+++ front/src/components/AddRecipe/AddRecipe.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Style from './AddRecipe.module.scss';
 import PageTitle from '../PageTitle/PageTitle';
+import Ingredients from './Ingredients/Ingredients';
 
 const AddRecipe = () => (
   <main className={Style.content}>
     <PageTitle>Добавить новый рецепт</PageTitle>
+    <Ingredients />
   </main>
 );
 
---
+++ front/src/components/AddRecipe/Ingredients/Ingredient/Ingredient.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Style from './Ingredient.module.scss';
+
+const Ingredient = () => (
+  <div className={Style.content}>
+    <p>Ingredient</p>
+  </div>
+);
+
+export default Ingredient;

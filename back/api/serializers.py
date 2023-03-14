7c87644776211fd1faa1f2191bd551cea0fb834c
Hromz <hromizz@gmail.com>
2023-03-14 16:48:39 +0300

    Kaiten_api

--- back/api/serializers.py
+++ back/api/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from recipe.models import *
-from collections import defaultdict
+from api.models import *
 from django.db import IntegrityError
 from rest_framework.exceptions import APIException
+
 
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,6 +16,7 @@ class RecipeIngredientAlternative:
         model = RecipeIngredients
         fields = ('ingredient_alternative', 'ingredient_alternative_volume', 'ingredient_alternative_measure')
 
+
 class RecipeIngredientAlternative(serializers.ModelSerializer):
     ingredient = serializers.CharField(source='ingredient_alternative')
     volume = serializers.IntegerField(source='ingredient_alternative_volume')
@@ -22,18 +24,20 @@ class RecipeIngredientAlternative(serializers.ModelSerializer):
 
     class Meta:
         model = RecipeIngredients
-        fields = ('ingredient', 'volume', 'measure')
+        fields = ['ingredient', 'volume', 'measure']
+
 
 class RecipeIngredientSerializer(serializers.ModelSerializer):
-    replacement = serializers.SerializerMethodField()
+    replacement = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = RecipeIngredients
         fields = ('ingredient', 'volume', 'measure', 'ingredient', 'replacement')
 
     def get_replacement(self, recipe_instance):
-        query_datas = RecipeIngredients.objects.select_related('ingredient', 'recipe').filter(
-        recipe=recipe_instance.recipe_id, ingredient=recipe_instance.ingredient_id).values('ingredient_alternative', 'ingredient_alternative_volume', 'ingredient_alternative_measure')
-
+        query_datas = RecipeIngredients.objects.select_related('recipe', 'ingredient').filter(
+        recipe=recipe_instance.recipe_id, ingredient=recipe_instance.ingredient_id).values('ingredient_alternative',
+                                                                                           'ingredient_alternative_volume',
+                                                                                           'ingredient_alternative_measure')
         if query_datas[0]['ingredient_alternative'] is not None:
             return RecipeIngredientAlternative(query_datas[0]).data
         return None
@@ -68,15 +72,15 @@ class RecipeDisplaySerializer(serializers.ModelSerializer):
         fields = ['name', 'description', 'complexity', 'cooking_time', 'oven_time', 'ingredients', 'steps', 'photos']
 
     def get_ingredients(self, recipe_instance):
-        query_datas = RecipeIngredients.objects.select_related('ingredient', 'recipe').filter(recipe=recipe_instance).all()#values()#'ingredient_id', 'ingredient__name', 'volume', 'measure')
+        query_datas = RecipeIngredients.objects.select_related('ingredient', 'recipe').filter(recipe=recipe_instance).all()
         return [RecipeIngredientSerializer(ingredient).data for ingredient in query_datas]
 
     def get_steps(self, recipe_instance):
-        query_datas = RecipeSteps.objects.select_related('step').filter(recipe=recipe_instance).values('step__step_number', 'step__description', 'step__photo')
+        query_datas = RecipeSteps.objects.select_related('step', 'recipe').filter(recipe=recipe_instance).values('step__step_number', 'step__description', 'step__photo')
         return [RecipeStepSerializer(step).data for step in query_datas]
 
     def get_photos(self, recipe_instance):
-        query_datas = RecipePhotos.objects.select_related('photo').filter(recipe=recipe_instance).values('photo__photo')
+        query_datas = RecipePhotos.objects.select_related('photo', 'recipe').filter(recipe=recipe_instance).values('photo__photo')
         return [RecipePhotoSerializer(photo).data for photo in query_datas]
 
 ##################################################
@@ -103,7 +107,6 @@ class RecipePhotoCreateSerializer(serializers.ModelSerializer):
 
 
 class RecipeIngredientAlternativeCreateSerializer(serializers.ModelSerializer):
-    #ingredient = IngredientSerializer(read_only=True)
     ingredient_id = serializers.PrimaryKeyRelatedField(write_only=True, source='ingredient', queryset=Ingredient.objects.all())
     volume = serializers.IntegerField(source='ingredient_alternative_volume')
     measure = serializers.CharField(source='ingredient_alternative_measure')
@@ -141,7 +144,8 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
 
             for ingredient_data in ingredients_data:
                 if ingredient_data.get('replacement') is None:
-                    RecipeIngredients.objects.create(recipe=recipe, ingredient=ingredient_data.get('ingredient'), volume=ingredient_data.get('volume'),
+                    RecipeIngredients.objects.create(recipe=recipe, ingredient=ingredient_data.get('ingredient'),
+                                                    volume=ingredient_data.get('volume'),
                                                     measure=ingredient_data.get('measure'), ingredient_alternative=None,
                                                     ingredient_alternative_volume=None, ingredient_alternative_measure=None)
                 else:
@@ -157,8 +161,92 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
             for photo_data in photos_data:
                 photo_to_add = Photo.objects.create(photo=photo_data.get('photo'))
                 RecipePhotos.objects.create(recipe=recipe, photo=photo_to_add)
+
         except IntegrityError as e:
             raise APIException(detail=e)
 
         return recipe
 
+
+##################################################################################
+#                          Participant post and put method's                     #
+#                                                                                #
+##################################################################################
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    kaiten_task_id = serializers.IntegerField()
+    name = serializers.CharField()
+    status = serializers.CharField()
+    description = serializers.CharField(allow_null=True)
+    update_date = serializers.DateTimeField()
+    create_date = serializers.DateTimeField()
+
+    class Meta:
+        model = Task
+        fields = ('kaiten_task_id', 'name', 'status', 'description', 'update_date', 'create_date')
+
+    def update(self, instance, validated_data):
+        instance.status = validated_data.get('status', instance.status)
+        instance.update_date = validated_data.get('update_date', instance.update_date)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        return instance
+
+
+class ParticipantCreateSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True)
+
+    class Meta:
+        model = Participant
+        fields = ('name', 'username', 'kaiten_user_id', 'number_of_completed_tasks', 'last_updated_task', 'tasks')
+
+    def create(self, validated_data):
+        tasks = validated_data.pop('tasks')
+        participant, created = Participant.objects.get_or_create(**validated_data)
+
+        for task in tasks:
+            task_to_add, created = Task.objects.get_or_create(kaiten_task_id=task.get('kaiten_task_id'),
+                                                              defaults={
+                                                                  'kaiten_task_id': task.get('kaiten_task_id'),
+                                                                  'name': task.get('name'),
+                                                                  'status': task.get('status'),
+                                                                  'update_date': task.get('update_date'),
+                                                                  'create_date': task.get('create_date')})
+
+            ParticipantTasks.objects.create(participant=participant, task=task_to_add)
+
+        return participant
+
+##################################################################################
+#                                                                                #
+#                          Participant get method                                #
+#                                                                                #
+##################################################################################
+
+
+class ParticipantTasksSerializer(serializers.ModelSerializer):
+    kaiten_task_id = serializers.IntegerField(source='task__kaiten_task_id')
+    name = serializers.CharField(source='task__name')
+    status = serializers.CharField(source='task__status')
+    description = serializers.CharField(source='task__description')
+    update_date = serializers.DateTimeField(source='task__update_date')
+    create_date = serializers.DateTimeField(source='task__create_date')
+
+    class Meta:
+        model = ParticipantTasks
+        fields = ('kaiten_task_id', 'name', 'status', 'description', 'update_date', 'create_date')
+
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    tasks = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Participant
+        fields = ('name', 'username', 'kaiten_user_id', 'number_of_completed_tasks', 'last_updated_task', 'tasks')
+
+    def get_tasks(self, participant_instance):
+        query_datas = ParticipantTasks.objects.select_related('participant', 'task').filter(participant=participant_instance).values('task__kaiten_task_id',
+                                                                                                                        'task__name', 'task__status',
+                                                                                                                        'task__update_date', 'task__create_date', 'task__description')
+        return [ParticipantTasksSerializer(task).data for task in query_datas]
--- back/api/urls.py
+++ back/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('recipes/', RecipeList.as_view()),
     path('recipe/<int:id>/', RecipeDetail.as_view()),
     path('ingredient/<int:id>/', IngredientDetail.as_view()),
-
+    path('kaiten/', KaitenData.as_view()),
 ]
 

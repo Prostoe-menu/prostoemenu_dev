039bc45614f78ff23f7943541167bb43c00d1780
Hromz <hromizz@gmail.com>
2023-03-15 08:46:39 +0300

    correct git push Kaiten API

--- back/api/models.py
+++ back/api/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Task(models.Model):
+    kaiten_task_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=350)
+    status = models.CharField(max_length=50)
+    description = models.TextField(null=True)
+    create_date = models.DateTimeField()
+    update_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
+
+
+class Participant(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    username = models.CharField(max_length=100, unique=True)
+    kaiten_user_id = models.IntegerField(null=True)
+    number_of_completed_tasks = models.IntegerField(default=0)
+    last_updated_task = models.DateTimeField()
+
+    task = models.ManyToManyField(Task, through='ParticipantTasks', related_name='participant_task')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Участник'
+        verbose_name_plural = 'Участники'
+        unique_together = ['name', 'username']
+
+
+class ParticipantTasks(models.Model):
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE, verbose_name='Номер участника')
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, verbose_name='Номер задания')
+
+    def __str__(self):
+        return self.participant.__str__() + " " + self.task.__str__()
+
+    class Meta:
+        verbose_name = 'Задание участника'
+        verbose_name_plural = 'Задания участника'
+        unique_together = ['participant', 'task']
--- back/api/views.py
+++ back/api/views.py
@@ -1,11 +1,14 @@
+import requests
 from rest_framework import status
 from rest_framework.decorators import APIView
 from rest_framework.generics import get_object_or_404
 from rest_framework.settings import api_settings
 from recipe.models import Recipe, Ingredient
-from .serializers import RecipeDisplaySerializer, RecipeCreateSerializer, IngredientSerializer
+from .models import Participant, Task
+from .serializers import RecipeDisplaySerializer, RecipeCreateSerializer, IngredientSerializer, TaskSerializer, \
+    ParticipantCreateSerializer, ParticipantSerializer
 from rest_framework.response import Response
-
+from datetime import datetime, timedelta
 
 class RecipeList(APIView):
     def get(self, request, format=None):
@@ -59,3 +62,82 @@ class IngredientDetail(APIView):
             return Response(serializer.data, status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class KaitenData(APIView):
+    def get(self, request, format=None):
+        participants = Participant.objects.all()
+        serializer = ParticipantSerializer(participants, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        auth_key = 'Bearer 18d47ac2-41af-4ba9-8501-4fb4a2586a3b'
+        url = 'https://boyarkin.kaiten.ru/api/latest/cards/?additional_card_fields=description'
+        r = requests.get(url, headers={'Accept': 'application/json', 'Content-Type': 'application/json',
+                                       'Authorization': auth_key})
+        cards = r.json()
+        participants = dict()
+
+        for card in cards:
+            if 'members' in card and card['column']['title'] in ['In progress', 'Ready (на проверку)', 'done']:
+                for member in card['members']:
+                    if member['username'] not in participants:
+                        participants[member['username']] = {'name': member['full_name'],
+                                                             'username': member['username'],
+                                                            'kaiten_user_id': member['id'],
+                                                             'last_updated_task': '', 'number_of_completed_tasks': 0,
+                                                             'tasks': []}
+                    participants[member['username']]['tasks'].append({'kaiten_task_id': card['id'],
+                                                                       'name': card['title'],
+                                                                       'status': card['column']['title'],
+                                                                       'description': card['description'],
+                                                                       'update_date': member['updated'],
+                                                                       'create_date': member['created']})
+
+                    participants[member['username']]['last_updated_task'] = max(
+                        participants[member['username']]['last_updated_task'], member['updated'])
+
+                    if card['column']['title'] == 'done':
+                        participants[member['username']]['number_of_completed_tasks'] = \
+                            participants[member['username']]['number_of_completed_tasks'] + 1
+
+        for participant in participants:
+            serializer = ParticipantCreateSerializer(data=participants[participant])
+
+            if serializer.is_valid():
+                serializer.save()
+
+        return Response(participants, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        auth_key = 'Bearer 18d47ac2-41af-4ba9-8501-4fb4a2586a3b'
+        url = 'https://boyarkin.kaiten.ru/api/latest/cards/?additional_card_fields=description&updated_after=' + str(datetime.now() - timedelta(minutes=2))
+        r = requests.get(url, headers={'Accept': 'application/json', 'Content-Type': 'application/json',
+                                       'Authorization': auth_key})
+        cards = r.json()
+        updated_cards = []
+        for card in cards:
+            try:
+                instance = Task.objects.get(kaiten_task_id=card['id'])
+
+                if instance:
+                    updated_cards.append(
+                        {'kaiten_task_id': card['id'], 'name': card['title'], 'status': card['column']['title'],
+                         'description': card['description'],
+                         'create_date': card['created'],
+                         'update_date': card['updated']})
+
+                    serializer = TaskSerializer(data={'kaiten_task_id': card['id'], 'name': card['title'], 'status': card['column']['title'],
+                                                      'description': card['description'],
+                                                      'create_date': card['created'],
+                                                      'update_date': card['updated']}, instance=instance)
+
+                    if serializer.is_valid():
+                        serializer.save()
+                    else:
+                        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            except:
+                updated_cards.append({'kaiten_task_id': card['id'], 'name': card['title'],'description': card['description'], 'status': 'Innactive DB entry'})
+
+        return Response(updated_cards, status=status.HTTP_200_OK)

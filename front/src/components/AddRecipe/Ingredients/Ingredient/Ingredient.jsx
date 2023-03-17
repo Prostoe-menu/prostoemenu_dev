6d8bc8b25059666ffc9072a6bc3fbd52472dd89a
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-03-17 22:53:07 +0100

    feat: handle adding and removing ingredient substitute

--- front/src/components/AddRecipe/Ingredients/Ingredient/Ingredient.jsx
+++ front/src/components/AddRecipe/Ingredients/Ingredient/Ingredient.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CloseButton from '../../../CloseButton/CloseButton';
 import Style from './Ingredient.module.scss';
 import InputsContainer from './InputsContainer/InputsContainer';
 
-const Ingredient = () => (
-  <div className={Style.content}>
-    <div className={Style.ingredient}>
-      <InputsContainer />
-      <CloseButton ariaLabelText="Удалить ингредиент" />
+const Ingredient = () => {
+  const [isSubstituteHidden, setIsSubstituteHidden] = useState(true);
+  const showSubstitute = () => {
+    setIsSubstituteHidden(!isSubstituteHidden);
+  };
+  return (
+    <div className={Style.content}>
+      <div className={Style.ingredient}>
+        <InputsContainer />
+        <CloseButton ariaLabelText="Удалить ингредиент" />
+      </div>
+      <button
+        className={`${Style.button} ${
+          !isSubstituteHidden && Style.button_substitute
+        }`}
+        type="button"
+        aria-label="Открыть поле для добавления варианта замены"
+        onClick={showSubstitute}
+      >
+        Вариант замены
+      </button>
+      <InputsContainer isHidden={isSubstituteHidden} isSubstitute />
     </div>
-    <button
-      type="button"
-      aria-label="Открыть поле для добавления варианта замены"
-      className={Style.button}
-    >
-      Вариант замены
-    </button>
-  </div>
-);
+  );
+};
 
 export default Ingredient;
--- front/src/components/AddRecipe/Ingredients/Ingredient/InputsContainer/InputsContainer.jsx
+++ front/src/components/AddRecipe/Ingredients/Ingredient/InputsContainer/InputsContainer.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import Style from './InputsContainer.module.scss';
 
-const Ingredient = () => (
-  <div className={Style.container}>
+const Ingredient = ({ isHidden, isSubstitute = false }) => (
+  <div
+    className={`${Style.container} ${
+      isSubstitute && isHidden && Style.container_hidden
+    }`}
+  >
     <input
       className={`${Style.input} ${Style.input_type_name}`}
-      name="ingredientName"
+      name={isSubstitute ? 'substituteName' : 'ingredientName'}
       placeholder="Начните вводить название"
     />
     <input
       className={`${Style.input} ${Style.input_type_quantity}`}
-      name="ingredientQuantity"
+      name={isSubstitute ? 'substituteQuantity' : 'ingredientQuantity'}
       type="number"
       placeholder="0"
     />
     <div className={Style.dropdownContainer}>
       <select
         className={`${Style.input} ${Style.input_type_dropdown}`}
-        name="ingredientMeasure"
+        name={isSubstitute ? 'substituteMeasure' : 'ingredientMeasure'}
       >
         <option value="gram">г</option>
         <option value="gram">г</option>

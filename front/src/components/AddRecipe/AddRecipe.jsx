40631b08afe00f5bd9b2db97505a514a2516f778
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-03-20 18:31:11 +0100

    ingredients wip

--- front/src/components/AddRecipe/AddRecipe.jsx
+++ front/src/components/AddRecipe/AddRecipe.jsx
@@ -3,15 +3,17 @@ import Style from './AddRecipe.module.scss';
 
 import PageTitle from '../PageTitle/PageTitle';
 import RecipeBox from '../RecipeBox/RecipeBox';
-import RecipeTitle from '../RecipeTitle/RecipeTitle';
-import MainInfo from '../MainInfo/MainInfo';
+// import RecipeTitle from '../RecipeTitle/RecipeTitle';
+// import MainInfo from '../MainInfo/MainInfo';
+import Ingredients from './Ingredients/Ingredients';
 
 const AddRecipe = () => (
   <main className={Style.content}>
     <PageTitle>Добавить новый рецепт</PageTitle>
     <RecipeBox>
-      <RecipeTitle>Основная информация *</RecipeTitle>
-      <MainInfo />
+      {/* <RecipeTitle>Основная информация *</RecipeTitle> */}
+      {/* <MainInfo /> */}
+      <Ingredients />
     </RecipeBox>
   </main>
 );
--- front/src/components/AddRecipe/Ingredients/Ingredients.jsx
+++ front/src/components/AddRecipe/Ingredients/Ingredients.jsx
@@ -1,36 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Style from './Ingredients.module.scss';
 import Ingredient from './Ingredient/Ingredient';
 import RecipeTitle from '../../RecipeTitle/RecipeTitle';
 import Button from '../../Button/Button';
 import addIcon from '../../../img/add.svg';
 
-const Ingredients = () => (
-  <section className={Style.content}>
-    <RecipeTitle>Ингредиенты*</RecipeTitle>
-    <div className={Style.textContainer}>
-      <p className={Style.text}>
-        Добавьте ингредиенты для вашего блюда, укажите их количество.
-      </p>
-      <p className={Style.text}>
-        Если ингредиент можно заменить другим, укажите это в поле «Вариант
-        замены».
-      </p>
-    </div>
-    <div className={Style.ingredientsContainer}>
-      <Ingredient />
-    </div>
-    <Button
-      btnClassName="button_border_grey"
-      isSubmit={false}
-      isDisabled={false}
-      // onClickBtn,
-      ariaLabelText="Добавить ингредиент"
-    >
-      <img className={Style.icon} src={addIcon} alt="Иконка 'плюсик'" />
-      Добавить ингредиент
-    </Button>
-  </section>
-);
+const Ingredients = () => {
+  const ingredients = [];
+  const [initialStep, setInitialStep] = useState(1);
+  return (
+    <section className={Style.ingredients}>
+      <RecipeTitle>Ингредиенты*</RecipeTitle>
+      <div className={Style.textContainer}>
+        <p className={Style.text}>
+          Добавьте ингредиенты для вашего блюда, укажите их количество.
+        </p>
+        <p className={Style.text}>
+          Если ингредиент можно заменить другим, укажите это в поле «Вариант
+          замены».
+        </p>
+      </div>
+      <ul className={Style.ingredients__list}>
+        {ingredients.map(() => (
+          <li>
+            <Ingredient />
+          </li>
+        ))}
+      </ul>
+      <Button
+        btnClassName="button_border_grey"
+        isSubmit={false}
+        isDisabled={false}
+        // onClickBtn,
+        ariaLabelText="Добавить ингредиент"
+      >
+        <img className={Style.icon} src={addIcon} alt="Иконка 'плюсик'" />
+        Добавить ингредиент
+      </Button>
+    </section>
+  );
+};
 
 export default Ingredients;

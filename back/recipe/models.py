722c9cf75d9779ff1f2cb2478d52c59c9e040f95
asb <asbabushkin@yandex.ru>
2023-03-23 12:51:08 +0500

    Recipe.User_id error fixed

--- back/recipe/models.py
+++ back/recipe/models.py
@@ -140,6 +140,7 @@ class Recipe(models.Model):
         default=False, verbose_name='Проверено')
     user = models.ForeignKey(
         User, null=True,
+        db_column='user',
         on_delete=models.SET_NULL,
         verbose_name='Id автора',
         related_name='recipes')

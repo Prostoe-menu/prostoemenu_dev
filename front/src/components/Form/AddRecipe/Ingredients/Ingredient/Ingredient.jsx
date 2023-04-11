c93d089184a2277294b9c7069726260c2b9f3f15
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-04-11 18:50:05 +0200

    Feature/ingredients (#55)

---
+++ front/src/components/Form/AddRecipe/Ingredients/Ingredient/Ingredient.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CloseButton from '../../../../UI/CloseButton/CloseButton';
+import Style from './Ingredient.module.scss';
+import InputsContainer from './InputsContainer/InputsContainer';
+
+const Ingredient = ({ hideButton, deleteIngredient }) => (
+  <div className={Style.content}>
+    <div className={Style.ingredient}>
+      <InputsContainer />
+      <CloseButton
+        hideButton={hideButton}
+        ariaLabelText="Удалить ингредиент"
+        onClose={deleteIngredient}
+      />
+    </div>
+  </div>
+);
+
+export default Ingredient;
---
+++ front/src/components/Form/AddRecipe/Ingredients/Ingredients.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import Style from './Ingredients.module.scss';
+import Ingredient from './Ingredient/Ingredient';
+import RecipeTitle from '../../RecipeTitle/RecipeTitle';
+import Button from '../../../UI/Button/Button';
+import addIcon from '../../../../images/add.svg';
+
+const Ingredients = () => {
+  const [ingredients, setIngredients] = useState([{ ingredient: '' }]);
+  const addIngredient = () => {
+    setIngredients([...ingredients, { id: uuidV4() }]);
+  };
+  const deleteIngredient = (id) => {
+    setIngredients(ingredients.filter((ingredient) => ingredient.id !== id));
+  };
+  return (
+    <section className={Style.ingredients}>
+      <RecipeTitle>
+        <span className={Style.mobileTitle}>2. </span>Ингредиенты*
+      </RecipeTitle>
+      <div className={Style.textContainer}>
+        <p className={Style.text}>
+          Добавьте ингредиенты для вашего блюда, укажите их количество.
+        </p>
+      </div>
+      <ul className={Style.ingredients__list}>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient.id}>
+            <Ingredient
+              hideButton={ingredients.length <= 1}
+              deleteIngredient={() => deleteIngredient(ingredient.id)}
+            />
+          </li>
+        ))}
+      </ul>
+      <Button
+        btnClassName="button_border_grey"
+        isSubmit={false}
+        isDisabled={false}
+        onClickBtn={addIngredient}
+        ariaLabelText="Добавить ингредиент"
+      >
+        <img className={Style.icon} src={addIcon} alt="Иконка 'плюсик'" />
+        Добавить ингредиент
+      </Button>
+    </section>
+  );
+};
+
+export default Ingredients;

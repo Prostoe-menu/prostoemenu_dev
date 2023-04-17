da0f3886f015af7db82fcfe33fc2833843f4e5e9
VladBorisof <vl-borisof@yandex.ru>
2023-04-17 09:40:47 +0300

    add Dockerfile

--- back/build_push_dockerhub.sh
+++ back/build_push_dockerhub.sh
@@ -1,7 +1,7 @@
 echo '* Building image...' && \
 docker build -t prostoemenu/prostoemenu:v0.1 .
 echo '* Logging to dockerhub...' && \
-docker login prostoemenu/prostoemenu --username prostoemenu --password-stdin 'yz$k4tDrb!GL'
+docker login --username prostoemenu --password ''
 echo '* Pushing image to dockerhub...' && \
 docker push prostoemenu/prostoemenu:v0.1
 echo '* Deleting image...' && \

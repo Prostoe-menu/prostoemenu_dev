44751d09233f6178d8b01dc92e03d3b558f29eef
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-05-04 17:55:49 +0200

    refactor: modal component is created with react portal

--- front/src/components/Modal/Modal.jsx
+++ front/src/components/Modal/Modal.jsx
@@ -1,30 +1,37 @@
 import React, { useState } from 'react';
+import ReactPortal from '../ReactPortal/ReactPortal';
 import Style from './Modal.module.scss';
 import CloseButton from '../UI/CloseButton/CloseButton';
 import ConfirmModal from './ConfirmModal/ConfirmModal';
 
-const Modal = () => {
-  // will be updated (depending on the case)
-  const [isModalOpen, setIsModalOpen] = useState(false);
+const Modal = ({ children }) => {
+  // add closing on escape key
+  const [isModalOpen, setIsModalOpen] = useState(true);
   const closeModal = () => setIsModalOpen(false);
 
+  if (!isModalOpen) return null;
+
   return (
-    <section className={`${Style.modal} ${isModalOpen && Style.openModal}`}>
-      <div
-        className={Style.overlay}
-        role="button"
-        tabIndex="0"
-        aria-label="Закрыть модальное окно"
-        onClick={closeModal}
-        onKeyDown={closeModal}
-      />
-      <div className={Style.content}>
-        <div className={Style.closeButtonContainer}>
-          <CloseButton sx={{ cursor: 'pointer' }} onClose={closeModal} />
+    <ReactPortal wrapperId="modal-container">
+      <section className={`${Style.modal} ${isModalOpen && Style.openModal}`}>
+        <div
+          className={Style.overlay}
+          role="button"
+          tabIndex="0"
+          aria-label="Закрыть модальное окно"
+          onClick={closeModal}
+          onKeyDown={closeModal}
+        />
+        <div className={Style.content}>
+          <div className={Style.closeButtonContainer}>
+            <CloseButton sx={{ cursor: 'pointer' }} onClose={closeModal} />
+          </div>
+          {children}
+          {/* Confirm Modal will be provided as a child when api post-request for creating a new recipe is successful */}
+          <ConfirmModal onClickClose={closeModal} />
         </div>
-        <ConfirmModal onClickClose={closeModal} />
-      </div>
-    </section>
+      </section>
+    </ReactPortal>
   );
 };
 

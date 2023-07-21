efd72a7aa0e0782d6e89ed8846996e2479c4bc53
Dmitry Nekrasov <demonekr@yandex.ru>
2023-07-21 19:56:32 +0300

    change readme

--- back/prostoemenu/settings/production.py
+++ back/prostoemenu/settings/production.py
@@ -18,7 +18,7 @@ DATABASES = {
       'ENGINE': os.getenv(
           'DB_ENGINE', default='django.db.backends.postgresql'
       ),
-      'NAME': os.getenv('POSTGRES_DB', default='prostoemenu'),
+      'NAME': os.getenv('POSTGRES_DB', default='postgres'),
       'USER': os.getenv('POSTGRES_USER', default='postgres'),
       'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
       'HOST': os.getenv('DB_HOST', default='db'),

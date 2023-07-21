8ed5a54f57a4bf74daffe8a2e64f617416651fcb
Dmitry Nekrasov <demonekr@yandex.ru>
2023-07-21 15:48:13 +0300

    fix prod settings

--- back/prostoemenu/settings/production.py
+++ back/prostoemenu/settings/production.py
@@ -2,12 +2,11 @@ import os
 
 from .base import *
 
-
 DEBUG = False
 
 ALLOWED_HOSTS += [
     'web',
-    '*'
+    '127.0.0.1'
 ]
 
 INSTALLED_APPS += [
@@ -19,9 +18,9 @@ DATABASES = {
       'ENGINE': os.getenv(
           'DB_ENGINE', default='django.db.backends.postgresql'
       ),
-      'NAME': os.getenv('DB_NAME', default='prostoemenu'),
+      'NAME': os.getenv('POSTGRES_DB', default='prostoemenu'),
       'USER': os.getenv('POSTGRES_USER', default='postgres'),
-      'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=''),
+      'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
       'HOST': os.getenv('DB_HOST', default='db'),
       'PORT': os.getenv('DB_PORT', default='5432')
   }

54cc2a09c1261d2b39d25a4551285a4540119872
javascriptnoob <timur.bobylev@yandex.ru>
2023-08-03 00:38:24 +0200

    i am trying to write skeleton for main page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,12 +7,14 @@ import Footer from './components/Layout/Footer/Footer';
 
 import Modal from './components/Modal/Modal';
 import ToastNotifications from './components/Toast/ToastNotifications';
+import HomePage from './pages/home-page/HomePage';
 
 const App = () => (
   <div className={Style.app}>
     <Header />
     <Routes>
-      <Route path="/" element={<AddRecipeForm />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/catalog" element={<AddRecipeForm />} />
     </Routes>
     <Modal />
     <Footer />
---
+++ front/src/pages/home-page/HomePage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HomePage = () => <div>Здесь будет собрана страница с рецептами</div>;
+
+export default HomePage;

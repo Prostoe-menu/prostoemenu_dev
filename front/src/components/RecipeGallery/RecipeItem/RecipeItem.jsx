fb3ac2d3f1f6e01d96f7ccb5eec886238d2f88fd
javascriptnoob <timur.bobylev@yandex.ru>
2023-08-10 21:42:51 +0200

    resolving in process

--- front/src/components/RecipeGallery/RecipeItem/RecipeItem.jsx
+++ front/src/components/RecipeGallery/RecipeItem/RecipeItem.jsx
@@ -2,21 +2,16 @@ import React from 'react';
 
 import styles from './RecipeItem.module.css';
 
-const RecipeItem = ({ recipe }) => {
-
-
-  return (
-    <div className={styles.wrapper}>
-      <div>
-        {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-        <img
-          src="https://pixfeeds.com/images/recipes/pork/1280-611764860-potato-stew-with-pork-and-mushrooms.jpg"
-          alt="image"
-          className={styles.imageContainer}
-        />
-        <h3 className={styles.name}>{recipe.name}</h3>
-      </div>
+const RecipeItem = ({ recipe }) => (
+  <div className={styles.wrapper}>
+    <div>
+      <img
+        src="https://pixfeeds.com/images/recipes/pork/1280-611764860-potato-stew-with-pork-and-mushrooms.jpg"
+        className={styles.imageContainer}
+        alt="блюдо"
+      />
+      <h3 className={styles.name}>{recipe.name}</h3>
     </div>
-  );
-};
+  </div>
+);
 export default RecipeItem;

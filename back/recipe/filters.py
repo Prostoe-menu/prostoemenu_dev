fc4b0ecf5a2e87e91060a87e541ddef78b71273c
Dmitry <demonekr@yandex.ru>
2023-08-31 21:58:08 +0300

    create filter by name for ingredient

--- back/recipe/filters.py
+++ back/recipe/filters.py
@@ -1,10 +1,11 @@
-from recipe.models import Ingredient
 import django_filters
+
+from recipe.models import Ingredient
 
 
 class BaseIngdredientFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr="istartswith")
-    
+
     class Meta:
         model = Ingredient
-        fields = ('name',)
+        fields = ("name",)

030d71d45723d1b10550218d7ba5abe03b7aeda9
Dmitry <demonekr@yandex.ru>
2023-08-31 21:58:52 +0300

    create func to insert inline_serializer into serializers

---
+++ back/recipe/utils.py
@@ -0,0 +1,15 @@
+import ulid
+from rest_framework import serializers
+
+
+def create_serializer_class(name, fields):
+    return type(name, (serializers.Serializer,), fields)
+
+
+def inline_serializer(*, fields, data=None, **kwargs):
+    serializer_class = create_serializer_class(name=str(ulid.new()), fields=fields)
+
+    if data is not None:
+        return serializer_class(data=data, **kwargs)
+
+    return serializer_class(**kwargs)

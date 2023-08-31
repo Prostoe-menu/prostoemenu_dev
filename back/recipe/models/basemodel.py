37f05181c7d710f4096cd0a728227ede65227542
Dmitry <demonekr@yandex.ru>
2023-08-31 21:36:02 +0300

    create basemodel

--- back/recipe/models/basemodel.py
+++ back/recipe/models/basemodel.py
@@ -3,13 +3,12 @@ from django.utils import timezone
 
 
 class Basemodel(models.Model):
+    """Базовая абстрактная модель."""
+
     created_at = models.DateTimeField(
-        db_index=True,
-        default=timezone.now,
-        verbose_name='Дата создания')
-    updated_at = models.DateTimeField(
-        auto_now=True,
-        verbose_name='Дата обновления')
+        db_index=True, default=timezone.now, verbose_name="Дата создания"
+    )
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
 
     class Meta:
         abstract = True

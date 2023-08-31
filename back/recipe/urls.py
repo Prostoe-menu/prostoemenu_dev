d2503372f7c36ac7c400bacb3dd0c34477ab5ed6
Dmitry <demonekr@yandex.ru>
2023-08-31 22:08:31 +0300

    create urls for all enabled apis

--- back/recipe/urls.py
+++ back/recipe/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from back.recipe.apis import IngredientListApi, IngredientDetailApi, MeasurementListApi
 
+from recipe.apis import (
+    IngredientDetailApi,
+    IngredientListApi,
+    MeasurementListApi,
+    RecipeCreateApi,
+)
 
 urlpatterns = [
-    path("ingredients/", IngredientListApi.as_view(), name='ingredients_list'),
-    path("ingredients/<int:id>/", IngredientDetailApi.as_view(), name='ingredients_detail'),
-    path("measurements/", MeasurementListApi.as_view(), name='measurements_list'),
+    path("ingredients/", IngredientListApi.as_view(), name="ingredients_list"),
+    path(
+        "ingredients/<int:id>/", IngredientDetailApi.as_view(), name="ingredient_detail"
+    ),
+    path("measurements/", MeasurementListApi.as_view(), name="measurements_list"),
+    path("create/", RecipeCreateApi.as_view(), name="recipe_create"),
 ]

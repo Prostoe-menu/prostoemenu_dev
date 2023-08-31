39c2b1cdc6b0e832f2982ccd807229c539f765d6
Dmitry <demonekr@yandex.ru>
2023-08-31 21:37:04 +0300

    create ingredient model

--- back/recipe/models/ingredient.py
+++ back/recipe/models/ingredient.py
@@ -1,21 +1,23 @@
-from recipe.models.basemodel import Basemodel
 from django.db import models
+
+from recipe.models.basemodel import Basemodel
 
 
 class Ingredient(Basemodel):
+    """Модель ингредиентов."""
+
     name = models.CharField(
-        max_length=100,
-        verbose_name='Название ингредиента',
-        unique=True)
-    category = models.CharField(
-        max_length=100,
-        verbose_name='Категория ингредиента')
-    sort = models.PositiveSmallIntegerField(null=True, verbose_name='Порядок ингредиента')
+        max_length=100, verbose_name="Название ингредиента", unique=True
+    )
+    category = models.CharField(max_length=100, verbose_name="Категория ингредиента")
+    sort = models.PositiveSmallIntegerField(
+        null=True, verbose_name="Порядок ингредиента"
+    )
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Ингредиент'
-        verbose_name_plural = 'Ингредиенты'
+        verbose_name = "Ингредиент"
+        verbose_name_plural = "Ингредиенты"
         ordering = ["name"]

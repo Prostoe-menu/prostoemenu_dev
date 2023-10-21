a42bebdfc28bbfaa996a77ccf88f819cd77f800c
asb <asbabushkin@yandex.ru>
2023-10-21 17:12:07 +0500

    urls, views, serializers moved from api to recipe app

--- back/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RecipeList, RecipeDetail
-
-urlpatterns = [
-    path('recipes/', RecipeList.as_view()),
-    path('recipe/<int:id>/', RecipeDetail.as_view()),
-]
--- back/recipe/urls.py
+++ back/recipe/urls.py
@@ -1,6 +1,8 @@
+from .views import (RecipeList,
+                    RecipeDetail)
 from django.urls import path
-from .views import RecipeListView
 
 urlpatterns = [
-    path('', RecipeListView.as_view(), name='home'),
+    path('recipes/', RecipeList.as_view()),
+    path('recipe/<int:id>/', RecipeDetail.as_view()),
 ]

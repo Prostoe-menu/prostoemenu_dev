773265479b578044cf5c0dd271c18dacd672b07b
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-11-01 12:50:21 +0100

    #99-App-imports-refactoring

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Style from './App.module.scss';
-import Header from './components/Layout/Header/Header';
-import AddRecipeForm from './components/AddRecipeForm/AddRecipeForm';
-import Footer from './components/Layout/Footer/Footer';
-
-import Modal from './components/Modal/Modal';
-import ToastNotifications from './components/Toast/ToastNotifications';
-import HomePage from './pages/home-page/HomePage';
+import Header from 'components/Layout/Header/Header';
+import AddRecipeForm from 'components/AddRecipeForm/AddRecipeForm';
+import Footer from 'components/Layout/Footer/Footer';
+import Modal from 'components/Modal/Modal';
+import ToastNotifications from 'components/Toast/ToastNotifications';
+import HomePage from 'pages/home-page/HomePage';
+import styles from './App.module.scss';
 
 const App = () => (
-  <div className={Style.app}>
+  <div className={styles.app}>
     <Header />
     <Routes>
       <Route path="/catalog" element={<HomePage />} />

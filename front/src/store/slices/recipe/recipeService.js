9e00c1460f42d4213d2255ea0c3fa3ab3bad96f5
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-11-20 16:41:17 +0100

    #124-update-env-variables

--- front/src/store/slices/recipe/recipeService.js
+++ front/src/store/slices/recipe/recipeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = `${process.env.REACT_APP_API_URL}/recipes/`;
+const URL = `${import.meta.env.VITE_API_URL}/recipes/`;
 
 const getRecipes = async () => {
   const reponse = await axios({

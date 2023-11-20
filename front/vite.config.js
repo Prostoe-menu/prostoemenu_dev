2ff9f27eff307834cd49623e6c6eb37255b73a46
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-11-20 16:50:47 +0100

    #124-add-eslint-setup

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react(), eslint()],
   resolve: {
     alias: {
       src: '/src',

ad50bc545b33dbb07ae0939cad16576e063420fd
asb <asbabushkin@yandex.ru>
2023-11-20 15:56:36 +0500

    * fix indents in prostoemenu/urls.py

--- back/prostoemenu/urls.py
+++ back/prostoemenu/urls.py
@@ -9,12 +9,12 @@ from drf_spectacular.views import (SpectacularAPIView,
                                    SpectacularSwaggerView)
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('api/', include('api.urls')),
-                  path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-                  path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
-                  path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar

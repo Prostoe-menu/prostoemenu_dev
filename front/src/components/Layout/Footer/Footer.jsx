24a7a552d66bedffed6bb7921ed693e51ecf4809
Anna Zozulia <bbbom@mail.ru>
2023-11-20 14:53:47 +0200

    FRONTEND-#117-refactor-footer (#123)

--- front/src/components/Layout/Footer/Footer.jsx
+++ front/src/components/Layout/Footer/Footer.jsx
@@ -1,57 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Style from './Footer.module.scss';
+import { AGREEMENT_URL } from '../../../utils/constants';
 import Logo from '../Logo/Logo';
-import Menu from '../../Navigation/Menu/Menu';
+import styles from './Footer.module.scss';
 
-const Footer = () => {
-  const navigation = [
-    {
-      name: 'Каталог рецептов',
-      route: '/catalogue',
-      id: 1,
-    },
-    {
-      name: 'Наша команда',
-      route: '/team',
-      id: 2,
-    },
-  ];
-  return (
-    <footer className={Style.footer}>
-      <div className={Style.container}>
-        <Link className={Style.logoLink} to="/">
-          <Logo />
-        </Link>
-        <Menu navigation={navigation} />
-      </div>
-      <ul className={Style.list}>
-        <li className={Style.list__item}>
-          <a
-            className={Style.list__link}
-            href="*"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Пользовательское соглашение
-          </a>
-        </li>
-        <li className={Style.list__item}>
-          <a
-            className={Style.list__link}
-            href="*"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Политика конфиденциальности
-          </a>
-        </li>
-      </ul>
-      <p className={Style.text}>
-        &copy; Простое Меню, 2023. Все права защищены.
-      </p>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className={styles.footer}>
+    <Link className={styles.logo} to="/">
+      <Logo />
+    </Link>
+
+    <p className={styles.links}>
+      <a
+        className={styles.agreement}
+        href={AGREEMENT_URL}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Пользовательское соглашение
+      </a>
+      <span className={styles.copyright}>
+        &copy;&nbsp;Простое&nbsp;Меню, 2023. Все права защищены.
+      </span>
+    </p>
+  </footer>
+);
 
 export default Footer;

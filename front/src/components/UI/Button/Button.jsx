fa07dc83b4175678e65cb087bc6a31951b181748
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-11-29 17:52:23 +0100

    #140-add-documentation

--- front/src/components/UI/Button/Button.jsx
+++ front/src/components/UI/Button/Button.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import classnames from 'classnames';
 import styles from './Button.module.scss';
 
+/**
+ * Компонент кнопки (с иконкой и без).
+ * Адаптация стилей и логики происходит через пропсы. Содержимое кнопки передается через пропс children.
+ * По умолчанию имеет type 'button'
+ * */
+
 const Button = ({
   btnClassName,
   isSubmit,
   children,
-  isDisabled,
+  disabled,
   onClickBtn,
   ariaLabelText,
 }) => {
@@ -19,14 +25,11 @@ const Button = ({
     <button
       className={classnames(styles.button, styles[btnClassName])}
       type={isSubmit ? 'submit' : 'button'}
-      disabled={isDisabled}
+      disabled={disabled}
       onClick={isSubmit ? null : handleClickBtn}
       aria-label={ariaLabelText}
     >
-      {
-        // При создании кнопки сюда пишем текст и если нужно добавляем тег img с иконкой
-        children
-      }
+      {children}
     </button>
   );
 };

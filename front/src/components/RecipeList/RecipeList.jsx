042524c705aa08c90811ff377b8ecf463078c032
azozulya <bbbom@mail.ru>
2023-12-03 22:05:49 +0200

    #141-refactor-recipe-list

---
+++ front/src/components/RecipeList/RecipeList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import RecipeCard from './RecipeCard/RecipeCard';
+import styles from './RecipeList.module.scss';
+
+const RecipeList = ({ title, recipes }) => (
+  <>
+    {title && <h2 className={styles.title}>{title}</h2>}
+
+    {recipes.length && (
+      <section className={styles.recipeList}>
+        {recipes.map((recipe) => (
+          <RecipeCard recipe={recipe} key={uuidV4()} />
+        ))}
+      </section>
+    )}
+  </>
+);
+
+export default RecipeList;

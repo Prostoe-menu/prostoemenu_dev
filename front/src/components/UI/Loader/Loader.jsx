35b940d2af8fa91b40c01673bbe3ec68a53d1816
azozulya <bbbom@mail.ru>
2024-01-26 10:31:48 +0200

    #141-fix-loader-styles

--- front/src/components/UI/Loader/Loader.jsx
+++ front/src/components/UI/Loader/Loader.jsx
@@ -2,7 +2,11 @@ import styles from './Loader.module.scss';
 
 const Loader = () => {
   return (
-    <div className={styles.spinner}>
+    <div
+      className={styles.spinner}
+      aria-busy={'true'}
+      aria-describedby="Loading..."
+    >
       <div className={styles.border}>
         <div className={styles.core}></div>
       </div>

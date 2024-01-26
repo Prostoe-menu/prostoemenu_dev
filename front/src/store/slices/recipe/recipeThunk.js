8f1589b6ecf13b74a5d8bc6dea75ca9b7bdaae6f
azozulya <bbbom@mail.ru>
2024-01-26 10:37:06 +0200

    #141-delete-comments

--- front/src/store/slices/recipe/recipeThunk.js
+++ front/src/store/slices/recipe/recipeThunk.js
@@ -3,13 +3,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { FETCH_RECIPES_ERROR_MESSAGE } from 'utils/constants';
 import { RECIPES_LIST_URL } from 'utils/urls';
 
-// Replace 'your_api_endpoint' with the actual API endpoint
 const API_ENDPOINT = `${import.meta.env.VITE_API_URL}${RECIPES_LIST_URL}`;
 
 const fetchRecipes = createAsyncThunk('recipes/fetchRecipes', async () => {
   try {
     const response = await axios.get(API_ENDPOINT);
-    const originalRecipes = response.data.results; // Assuming the 'results' array contains the recipes
+    const originalRecipes = response.data.results;
     return [...Array(3)].map(() => originalRecipes).flat();
   } catch (error) {
     throw new Error(FETCH_RECIPES_ERROR_MESSAGE);

da6366d42934522f54f83012a1c4a81c9e6a59c3
azozulya <bbbom@mail.ru>
2024-02-08 11:16:47 +0200

    #158-pr-comments-fix

--- front/src/components/Recipe/elements/Ingredients/Ingredients.jsx
+++ front/src/components/Recipe/elements/Ingredients/Ingredients.jsx
@@ -4,11 +4,11 @@ export const Ingredients = ({ list }) => {
   return (
     <section className={styles.ingredientsWrapper}>
       <h2 className={styles.title}>Вам понадобится</h2>
-      <ul className={styles.inrgedients}>
+      <ul className={styles.ingredients}>
         {list.map((item) => (
           <li key={item.id} className={styles.ingredient}>
             <span>{item.name}</span>
-            <span className={styles.line}></span>
+            <span className={styles.divider}></span>
             <span className={styles.volume}>
               {item.volume} {item.measure_unit}
             </span>
--- front/src/store/slices/recipe/recipeThunk.js
+++ front/src/store/slices/recipe/recipeThunk.js
@@ -1,4 +1,3 @@
-//import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import {
   FETCH_RECIPES_ERROR_MESSAGE,

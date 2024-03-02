9ebd852af7a4eec6cd62f3e86e81f439664f3a5f
Dmitry <demonekr@yandex.ru>
2024-03-02 21:17:31 +0300

    remove useless paths

--- back/api/v1/urls.py
+++ back/api/v1/urls.py
@@ -3,10 +3,7 @@ from django.urls import (path,
 
 
 urlpatterns = [
-    path('measurements/', include('measurement.urls')),
-    path('ingredients/', include('ingredient.urls')),
+    path('measurements/', include('measurements.urls')),
+    path('ingredients/', include('ingredients.urls')),
     path('recipes/', include('recipe.urls')),
-    path('account/', include('account.urls')),
-    path('auth/', include('djoser.urls')),
-    path('kaiten/', include('kaiten.urls')),
 ]

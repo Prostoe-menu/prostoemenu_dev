a1517effcbada8d3f33b3e021095895e784bc40c
Dmitry <demonekr@yandex.ru>
2024-03-02 21:18:49 +0300

    create common app with custom abstract base model

---
+++ back/common/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class CustomBaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    class Meta:
+        abstract = True

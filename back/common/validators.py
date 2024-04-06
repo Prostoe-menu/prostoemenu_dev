4f39af1b6533a89473671c6cd0c6e1fdf7c555cc
asb <asbabushkin@yandex.ru>
2024-04-06 16:06:09 +0500

    * formatted by black & ruff

--- back/common/validators.py
+++ back/common/validators.py
@@ -2,7 +2,6 @@ from django.conf import settings as django_settings
 from django.core.exceptions import ValidationError
 
 
-
 def validate_accepted_symbols(value):
     for i in value:
         if i not in django_settings.ACCEPTED_SYMBOLS:

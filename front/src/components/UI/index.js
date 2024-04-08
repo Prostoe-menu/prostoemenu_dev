daa39aa754f5a66efcccfef209109bd25899fdf0
azozulya <bbbom@mail.ru>
2024-04-08 08:20:21 +0300

    #157-add-cooking-oven-time-ui

--- front/src/components/UI/index.js
+++ front/src/components/UI/index.js
@@ -1,3 +1,7 @@
 export { ErrorMessage } from './ErrorMessage/ErrorMessage';
 
 export { default as Loader } from './Loader/Loader';
+
+export { CookingTime } from './CookingTime/CookingTime';
+
+export { OvenTime } from './OvenTime/OvenTime';

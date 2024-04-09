9a4c05b15d1399eb64c0003e1066343cbddd3131
asb <asbabushkin@yandex.ru>
2024-04-09 19:05:18 +0500

    RecipeStep model test_only_accepted_symbols_in_step_descriprion() added.

--- back/recipes/tests/test_models.py
+++ back/recipes/tests/test_models.py
@@ -19,62 +19,49 @@ class RecipeStepTest(TestCase):
             quantity=2,
             complexity=1,
             author=None,
-            cover_path="data/default_photo.jpg",
+            cover_path="mediafiles/media/default_photo.jpg",
         )
 
     def test_step_number_cannot_be_less_than_1(self):
-        print("test_step_number_cannot_be_less_than_1")
         with self.assertRaises(ValidationError):
-            recipe_step = RecipeStep.objects.create(
+            RecipeStep.objects.create(
                 recipe=Recipe.objects.get(pk=1),
                 step_number=0,
                 description="Step 0 description",
-                image="data/default_photo.jpg",
+                image="mediafiles/media/default_photo.jpg",
             )
 
-            recipe_step.full_clean()
-
     def test_step_number_cannot_be_more_than_20(self):
-        print("test_step_number_cannot_be_more_than_20")
         with self.assertRaises(ValidationError):
-            recipe_step = RecipeStep.objects.create(
+            RecipeStep.objects.create(
                 recipe=Recipe.objects.get(pk=1),
                 step_number=21,
                 description="Step description",
-                image="data/default_photo.jpg",
+                image="mediafiles/media/default_photo.jpg",
             )
 
-            recipe_step.full_clean()
-
     def test_step_number_is_from_1_to_20(self):
-        print("test_step_number_is_from_1_to_20")
         valid_numbers = [1, 10, 20]
         for number in valid_numbers:
-            recipe_step = RecipeStep.objects.create(
+            RecipeStep.objects.create(
                 recipe=Recipe.objects.get(pk=1),
                 step_number=number,
                 description="Step description",
-                image="data/default_photo.jpg",
+                image="mediafiles/media/default_photo.jpg",
             )
 
-            recipe_step.full_clean()
-
     def test_description_cannot_be_shorter_then_10(self):
-        print("test_description_cannot_be_shorter_then_10")
         with self.assertRaises(ValidationError):
-            recipe_step = RecipeStep.objects.create(
+            RecipeStep.objects.create(
                 recipe=Recipe.objects.get(pk=1),
                 step_number=1,
                 description="descripti",
-                image="data/default_photo.jpg",
+                image="mediafiles/media/default_photo.jpg",
             )
 
-            recipe_step.full_clean()
-
     def test_description_cannot_be_longer_then_500(self):
-        print("test_description_cannot_be_longer_then_500")
         with self.assertRaises(ValidationError):
-            recipe_step = RecipeStep.objects.create(
+            RecipeStep.objects.create(
                 recipe=Recipe.objects.get(pk=1),
                 step_number=1,
                 description="".join(
@@ -83,26 +70,29 @@ class RecipeStepTest(TestCase):
                         for i in range(501)
                     ]
                 ),
-                image="data/default_photo.jpg",
+                image="mediafiles/media/default_photo.jpg",
             )
 
-            recipe_step.full_clean()
-
-    def test_step_numbers_cannot_be_duplicated(self):
-        print("test_step_numbers_cannot_be_duplicated")
+    def test_step_number_cannot_be_duplicated(self):
+        RecipeStep.objects.create(
+            recipe=Recipe.objects.get(pk=1),
+            step_number=1,
+            description="step description",
+            image="mediafiles/media/default_photo.jpg",
+        )
         with self.assertRaises(ValidationError):
             RecipeStep.objects.create(
                 recipe=Recipe.objects.get(pk=1),
                 step_number=1,
-                description="step description",
-                image="data/default_photo.jpg",
+                description="step description 2",
+                image="mediafiles/media/default_photo.jpg",
             )
 
-            same_number_step = RecipeStep.objects.create(
+    def test_only_accepted_symbols_in_step_descriprion(self):
+        with self.assertRaises(ValidationError):
+            RecipeStep.objects.create(
                 recipe=Recipe.objects.get(pk=1),
                 step_number=1,
-                description="step description 2",
-                image="data/default_photo.jpg",
+                description="step description with invalid symbol _ ",
+                image="mediafiles/media/default_photo.jpg",
             )
-
-            same_number_step.full_clean()

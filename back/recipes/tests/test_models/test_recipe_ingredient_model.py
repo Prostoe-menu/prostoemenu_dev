07c25d21bfee0565e510dc8333cb6fa1b935c49f
asb <asbabushkin@yandex.ru>
2024-04-26 15:39:16 +0500

    Models recipes.Category & ingredients.Category added to classes RecipeTest,  RecipeIngredientTest, RecipeStepTest

--- back/recipes/tests/test_models/test_recipe_ingredient_model.py
+++ back/recipes/tests/test_models/test_recipe_ingredient_model.py
@@ -4,16 +4,25 @@ from django.conf import settings as django_settings
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
+import ingredients.models
+import recipes.models
 from ingredients.models import Ingredient
 from measurements.models import Measurement
 from recipes.models import Recipe, RecipeIngredient
 
 
 class RecipeIngredientTest(TestCase):
-
     @classmethod
     def setUpTestData(cls):
-
+        recipe_category = recipes.models.Category.objects.create(
+            name="Основные блюда",
+            description="Описание категории рецептов Основные блюда",
+        )
+        ingr_category = ingredients.models.Category.objects.create(
+            name="Молочные продукты", description="Описание категории Молочные продукты"
+        )
+        Ingredient.objects.create(name="Молоко", category=ingr_category, sort=10)
+        Measurement.objects.create(name="стакан", abbreviation="ст.")
         Recipe.objects.create(
             title="Омлет по-берлински",
             description="Описание омлета по-берлински",
@@ -23,10 +32,8 @@ class RecipeIngredientTest(TestCase):
             complexity=django_settings.MIN_RECIPE_COMPLEXITY,
             author=None,
             cover_path="mediafiles/media/default_photo.jpg",
+            category=recipe_category,
         )
-
-        Ingredient.objects.create(name="Молоко", category="Молочные продукты", sort=10)
-        Measurement.objects.create(name="стакан", abbreviation="ст.")
 
     def test_ingredient_volume_cannot_be_less_than_MIN_INGREDIENT_VOLUME(self):
         with self.assertRaises(ValidationError):

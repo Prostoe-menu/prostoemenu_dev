92565ffe47043ec78f37d1f26a0cd15810227adc
asb <asbabushkin@yandex.ru>
2024-05-03 12:53:15 +0500

    Filling RecipeStep model script created.

--- back/recipes/management/commands/load_recipes.py
+++ back/recipes/management/commands/load_recipes.py
@@ -9,6 +9,29 @@ from ingredients.models import Ingredient
 from measurements.models import Measurement
 
 
+def add_recipe_step_objects(data, recipe_obj, model=RecipeStep):
+    recipe_step_objects = []
+    for row in data:
+        recipe_step_data = {
+            "recipe": recipe_obj,
+            "step_number": row["step_num"],
+            "description": row["step_text"],
+            "image": row["step_photo"]
+        }
+        recipe_step_objects.append(recipe_step_data)
+
+    recodered_objects = []
+    for obj in recipe_step_objects:
+        try:
+            step_obj = model.objects.create(**obj)
+            recodered_objects.append(step_obj)
+        except Exception as err:
+            print(f"Ошибка! Рецепт: {recipe_obj}. Данные шага: {obj} при сохранении recipe_step: {err}")
+            for obj in recodered_objects:
+                obj.delete()
+            return False
+    return recodered_objects
+
 
 def add_recipe_ingredient_objects(data, recipe_obj, model=RecipeIngredient):
     recipe_ingr_objects = []
@@ -37,7 +60,7 @@ def add_recipe_ingredient_objects(data, recipe_obj, model=RecipeIngredient):
             new_recipe_ingredient_obj = model.objects.create(**object)
             recodered_objects.append(new_recipe_ingredient_obj)
         except Exception as err:
-            print(f"Ошибка! {recipe_obj} {object} при сохранении объекта recipe_ingredient: {err}")
+            print(f"Ошибка! {recipe_obj} {object} при сохранении recipe_ingredient: {err}")
             for obj in recodered_objects:
                 obj.delete()
             return False
@@ -79,51 +102,7 @@ class Command(BaseCommand):
                 continue
 
             add_recipe_ingredient_objects(row["dish_data"]["ingr"], new_recipe_obj)
-
-
-
-
-
-
-
-
-            # for ingredient in row["dish_data"]["ingr"]:
-            #     recipe_ingredient_data = {
-            #         "recipe": new_recipe_obj,
-            #         "volume": ingredient["ingr_amount"],
-            #     }
-            #     try:
-            #         recipe_ingredient_data["ingredient"] = Ingredient.objects.get(
-            #             name=ingredient["ingr_name"]
-            #         )
-            #     except Ingredient.DoesNotExist as err:
-            #         print(
-            #             f"Ошибка! Ингредиент {ingredient['ingr_name']} отсутствует в БД. {err}"
-            #         )
-            #         continue
-            #
-            #     try:
-            #         # recipe_ingredient_data["measure"] = Measurement.objects.filter(name=ingredient["ingr_measure"])
-            #         recipe_ingredient_data["measure"] = Measurement.objects.get(
-            #             name=ingredient["ingr_measure"]
-            #         )
-            #     except Measurement.DoesNotExist as err:
-            #         print(
-            #             f"Ошибка! Единица измерения {ingredient['ingr_measure']} отсутствует в БД. {err}"
-            #         )
-            #         continue
-            #     models["recipe_ingredient_model"].objects.create(
-            #         **recipe_ingredient_data
-            #     )
-            #
-            # for step in row["dish_data"]["steps"]:
-            #     recipe_step_data = {
-            #         "recipe": new_recipe_obj,
-            #         "step_number": step["step_num"],
-            #         "description": step["step_text"],
-            #         "image": step["step_photo_title"],
-            #     }
-            #     models["recipe_step_model"].objects.create(**recipe_step_data)
+            add_recipe_step_objects(row["dish_data"]["steps"], new_recipe_obj)
 
         return f"Database Update {model}"
 

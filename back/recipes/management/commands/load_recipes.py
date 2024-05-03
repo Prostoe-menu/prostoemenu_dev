d15698ed482b93b6b9637d5f2ca1a414e1dcd225
asb <asbabushkin@yandex.ru>
2024-05-03 16:18:00 +0500

    Cleaning code.

--- back/recipes/management/commands/load_recipes.py
+++ back/recipes/management/commands/load_recipes.py
@@ -117,6 +117,6 @@ class Command(BaseCommand):
         return f"Database Update {model}"
 
     def handle(self, *args, **options):
-        with open("back/data/recipes.json", "rb") as recipes:
+        with open("data/recipes.json", "rb") as recipes:
             reader_recipes = json.load(recipes)
         self.stdout.write(self.style.SUCCESS(self.add_objects(Recipe, reader_recipes)))

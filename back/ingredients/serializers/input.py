71ce9dff9d876be86887a2289a05986dc0ee2f72
asb <asbabushkin@yandex.ru>
2024-06-04 11:47:02 +0500

    Ingredient search function ingredient_list() updated.

--- back/ingredients/serializers/input.py
+++ back/ingredients/serializers/input.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 
+from common.validators import validate_accepted_symbols
+
 
 class IngredientQueryInputSerializer(serializers.Serializer):
     """Сериализатор параметров запроса."""
 
-    name = serializers.CharField(required=False)
+    name = serializers.CharField(
+        required=False, min_length=3, validators=[validate_accepted_symbols]
+    )

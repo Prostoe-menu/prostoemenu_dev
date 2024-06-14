e05db45708ca78f9ae04550e33789f137a78da03
azozulya <bbbom@mail.ru>
2024-06-14 11:17:30 +0300

    #115-UI-move-to-shared/ui

--- front/src/components/UI/index.js
+++
@@ -1,7 +0,0 @@
-export { ErrorMessage } from './ErrorMessage/ErrorMessage';
-
-export { default as Loader } from './Loader/Loader';
-
-export { CookingTime } from './CookingTime/CookingTime';
-
-export { OvenTime } from './OvenTime/OvenTime';
---
+++ front/src/shared/ui/index.js
@@ -0,0 +1,9 @@
+export { ErrorMessage } from './ErrorMessage/ErrorMessage';
+
+export { default as Loader } from './Loader/Loader';
+
+export { CookingTime } from './CookingTime/CookingTime';
+
+export { OvenTime } from './OvenTime/OvenTime';
+
+export { default as Button } from './Button';

e639a3e61f52e423f688e0bf6ffe13ae7ebe2439
asb <asbabushkin@yandex.ru>
2024-06-21 09:54:08 +0500

    class Command(BaseCommand) rearranged.

--- back/recipes/management/commands/load_recipes.py
+++ back/recipes/management/commands/load_recipes.py
@@ -5,66 +5,16 @@ from django.conf import settings as django_settings
 from django.core.files.images import ImageFile
 from django.core.management.base import BaseCommand
 from django.db import transaction
-
 from ingredients.models import Ingredient
 from measurements.models import Measurement
 from recipes.models import Category, Recipe, RecipeIngredient, RecipeStep
 
 
 class Command(BaseCommand):
-
-    @staticmethod
-    def get_upload_folder(filename):
-        filename = filename.split("/")[-1]
-        return os.path.join("recipes", filename.split(".")[0])
-
-    @staticmethod
-    def rearrange_image_storage(recipe_obj):
-        old_dir = os.path.join(
-            django_settings.MEDIA_ROOT,
-            Command.get_upload_folder(recipe_obj.cover_path.path),
-        )
-        new_dir = os.path.join(
-            django_settings.MEDIA_ROOT, "recipes", str(recipe_obj.pk)
-        )
-        os.rename(old_dir, new_dir)
-        recipe_obj.cover_path.name = os.path.join(
-            "recipes", str(recipe_obj.pk), recipe_obj.cover_path.name.split("/")[-1]
-        )
-        recipe_obj.save()
-
-    @staticmethod
-    def get_image(link):
-        if not link:
-            return None
-        try:
-            return ImageFile(
-                open(
-                    f"{django_settings.IMAGE_SOURCE_FOLDER}/{link.split('/')[-1]}", "rb"
-                )
-            )
-        except FileNotFoundError:
-            return None
-
-    @staticmethod
-    def add_rec_step_objects(data, recipe_obj):
-        for row in data:
-            RecipeStep.objects.create(
-                recipe=recipe_obj,
-                step_number=row["step_num"],
-                description=row["step_text"],
-                image=Command.get_image(row["step_photo"]),
-            )
-
-    @staticmethod
-    def add_rec_ingr_objects(data, recipe_obj):
-        for row in data:
-            rec_ingr_data = {"recipe": recipe_obj, "volume": row["ingr_amount"]}
-            rec_ingr_data["ingredient"] = Ingredient.objects.get(name=row["ingr_name"])
-            rec_ingr_data["measure"] = Measurement.objects.get(
-                name=row["ingr_measure"].lower()
-            )
-            RecipeIngredient.objects.create(**rec_ingr_data)
+    def handle(self, *args, **options):
+        with open("back/data/recipes.json", "rb") as recipes:
+            reader_recipes = json.load(recipes)
+        self.stdout.write(self.style.SUCCESS(self.add_objects(Recipe, reader_recipes)))
 
     @staticmethod
     def add_objects(model, reader):
@@ -73,11 +23,7 @@ class Command(BaseCommand):
 
         for row in reader:
             image = Command.get_image(row["dish_data"]["main_photo"])
-            image_file = (
-                image
-                if image
-                else default_image
-            )
+            image_file = image if image else default_image
 
             recipe_data = {
                 "title": row["dish_name"],
@@ -116,7 +62,55 @@ class Command(BaseCommand):
 
         return f"Database Update {model}"
 
-    def handle(self, *args, **options):
-        with open("back/data/recipes.json", "rb") as recipes:
-            reader_recipes = json.load(recipes)
-        self.stdout.write(self.style.SUCCESS(self.add_objects(Recipe, reader_recipes)))
+    @staticmethod
+    def get_image(link):
+        if not link:
+            return None
+        try:
+            return ImageFile(
+                open(
+                    f"{django_settings.IMAGE_SOURCE_FOLDER}/{link.split('/')[-1]}", "rb"
+                )
+            )
+        except FileNotFoundError:
+            return None
+
+    @staticmethod
+    def rearrange_image_storage(recipe_obj):
+        old_dir = os.path.join(
+            django_settings.MEDIA_ROOT,
+            Command.get_upload_folder(recipe_obj.cover_path.path),
+        )
+        new_dir = os.path.join(
+            django_settings.MEDIA_ROOT, "recipes", str(recipe_obj.pk)
+        )
+        os.rename(old_dir, new_dir)
+        recipe_obj.cover_path.name = os.path.join(
+            "recipes", str(recipe_obj.pk), recipe_obj.cover_path.name.split("/")[-1]
+        )
+        recipe_obj.save()
+
+    @staticmethod
+    def get_upload_folder(filename):
+        filename = filename.split("/")[-1]
+        return os.path.join("recipes", filename.split(".")[0])
+
+    @staticmethod
+    def add_rec_ingr_objects(data, recipe_obj):
+        for row in data:
+            rec_ingr_data = {"recipe": recipe_obj, "volume": row["ingr_amount"]}
+            rec_ingr_data["ingredient"] = Ingredient.objects.get(name=row["ingr_name"])
+            rec_ingr_data["measure"] = Measurement.objects.get(
+                name=row["ingr_measure"].lower()
+            )
+            RecipeIngredient.objects.create(**rec_ingr_data)
+
+    @staticmethod
+    def add_rec_step_objects(data, recipe_obj):
+        for row in data:
+            RecipeStep.objects.create(
+                recipe=recipe_obj,
+                step_number=row["step_num"],
+                description=row["step_text"],
+                image=Command.get_image(row["step_photo"]),
+            )

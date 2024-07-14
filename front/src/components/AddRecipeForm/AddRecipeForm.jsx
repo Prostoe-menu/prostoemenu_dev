065b7995e7ce701ae804aa2d3701da174c9aa0dd
azozulya <bbbom@mail.ru>
2024-07-14 13:16:15 +0300

    #110-fix-styles

--- front/src/components/AddRecipeForm/AddRecipeForm.jsx
+++ front/src/components/AddRecipeForm/AddRecipeForm.jsx
@@ -5,23 +5,23 @@ import StageOne from 'components/AddRecipeForm/Stages/StageOne';
 import StageThree from 'components/AddRecipeForm/Stages/StageThree';
 import StageTwo from 'components/AddRecipeForm/Stages/StageTwo';
 import PageTitle from 'components/PageTitle/PageTitle';
-import Style from './AddRecipeForm.module.scss';
+import styles from './AddRecipeForm.module.scss';
 
 const AddRecipeForm = () => {
   const currentStage = useSelector((state) => state.form.currentFormStage);
 
   return (
-    <main className={Style.content}>
+    <>
       <PageTitle>Добавить новый рецепт</PageTitle>
 
-      <div className={Style.container}>
+      <section className={styles.container}>
         <ProgressBar activeStep={currentStage} />
         {currentStage === 1 && <StageOne />}
         {currentStage === 2 && <StageTwo />}
         {currentStage === 3 && <StageThree />}
         {currentStage === 4 && <StageFour />}
-      </div>
-    </main>
+      </section>
+    </>
   );
 };
 

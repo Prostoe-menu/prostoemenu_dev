81695bbdb3a2566dfbaad5b1423c2ac6155d26c7
asb <asbabushkin@yandex.ru>
2024-08-01 09:52:14 +0500

    Recipe_search_by_ingr() draft.

--- back/recipes/selectors.py
+++ back/recipes/selectors.py
@@ -12,20 +12,36 @@ def recipe_search_by_ingr(request_data):
         .filter(category__name__iexact="приправа")
         .values("name")
     )
-    ingr_excluded = {i["name"] for i in ingr_excluded}
+    ingr_excluded = set(i["name"] for i in ingr_excluded)
     ingr_lst = list(set(request_data["ingr"]).difference(ingr_excluded))
 
-    recipes = (
-        Recipe.objects.prefetch_related("ingredients__ingredient")
-        .filter(ingredients__ingredient__name__in=ingr_lst)
-        .annotate(
-            ingr_included=Count(
-                "pk",
-                filter=Q(ingredients__ingredient__name__in=ingr_lst),
-            )
-        )
-        .order_by("-ingr_included")
-    )
+
+    # recs = Recipe.objects.annotate(
+    #     num_ingr=Count("ingredients"),
+    #     num_ingr_clean=Count("ingredients", filter=~Q(ingredients__ingredient__name__in=ingr_excluded)),
+    #     ingr_included=Count("pk", filter=Q(ingredients__ingredient__name__in=ingr_lst))
+    # )
+    # for r in recs:
+    #     print(r.title, r.num_ingr, r.num_ingr_clean, r.ingr_included)
+
+
+    recipes = Recipe.objects.prefetch_related("ingredients").annotate(
+            ingr_total=Count("ingredients", distinct=True),
+            ingr_clean=Count("ingredients", filter=~Q(ingredients__ingredient__name__in=ingr_excluded), distinct=True),
+            ingr_included=Count("ingredients", filter=Q(ingredients__ingredient__name__in=ingr_lst), distinct=True),
+            ingr_delta=(Count("ingredients", filter=~Q(ingredients__ingredient__name__in=ingr_excluded), distinct=True) - Count("ingredients", filter=Q(ingredients__ingredient__name__in=ingr_lst), distinct=True)),
+        ).filter(ingredients__ingredient__name__in=ingr_lst).order_by("ingr_delta", "title")
+
+
+    # recipes = Recipe.objects.filter(ingredients__ingredient__name__in=ingr_lst)
+    # recipes = Recipe.objects.filter(pk__in=recipes).annotate(ingr_total=Count("ingredients"),
+    #      ingr_clean=Count("ingredients", filter=~Q(ingredients__ingredient__name__in=ingr_excluded)),
+    #      ingr_included=Count("pk", filter=Q(ingredients__ingredient__name__in=ingr_lst)),
+    # ).order_by("-ingr_included")
+
+    print(recipes.query)
+    for rec in recipes:
+        print(rec.title, rec.ingr_total, rec.ingr_clean, rec.ingr_included, rec.ingr_delta)
     return recipes
 
 

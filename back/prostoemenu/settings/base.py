6dde01f27785b0dc0b3bf5d3aba4eead4a91db22
asb <asbabushkin@yandex.ru>
2024-09-04 12:59:18 +0500

    STATICFILES_DIRS added.

--- back/prostoemenu/settings/base.py
+++ back/prostoemenu/settings/base.py
@@ -125,11 +125,14 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR.joinpath("static")
+STATIC_ROOT = BASE_DIR.joinpath("staticfiles")
+STATICFILES_DIRS = (
+    BASE_DIR.joinpath("static"),
+)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_ROOT = BASE_DIR.joinpath("media")
+MEDIA_ROOT = BASE_DIR.joinpath("mediafiles")
 MEDIA_URL = "/media/"
 
 DJOSER = {

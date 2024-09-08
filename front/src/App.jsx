707a1351216c94fae0d3159026732ef6ceaabca7
azozulya <bbbom@mail.ru>
2024-09-08 12:02:35 +0300

    196-refactor-routing

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,36 +1,44 @@
-import { lazy, Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 import Layout from 'components/Layout';
 import NotFound from 'components/NotFound';
 import HomePage from 'pages/HomePage';
 import Loader from 'ui/Loader';
 
-const AddRecipePage = lazy(() => import('pages/AddRecipePage'));
-const RecipePage = lazy(() => import('pages/RecipePage'));
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorBoundary />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/new-recipe',
+        async lazy() {
+          const { AddRecipePage } = await import('pages/AddRecipePage');
+          return { Component: AddRecipePage };
+        },
+      },
+      {
+        path: '/recipe/:id',
+        async lazy() {
+          const { RecipePage } = await import('pages/RecipePage');
+          return { Component: RecipePage };
+        },
+      },
+      {
+        path: '/*',
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
 
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Layout />}>
-      <Route index element={<HomePage />} />
-      <Route
-        path="/new-recipe"
-        element={
-          <Suspense fallback={<Loader />}>
-            <AddRecipePage />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/recipe/:id"
-        element={
-          <Suspense fallback={<Loader />}>
-            <RecipePage />
-          </Suspense>
-        }
-      />
-      <Route path="/*" element={<NotFound />} />
-    </Route>
-  </Routes>
+  <RouterProvider router={router} fallbackElement={<Loader />} />
 );
 
 export default App;

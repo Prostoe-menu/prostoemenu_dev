4605f46ce1ff815e8b9133d68e35f44b60cff274
asb <asbabushkin@yandex.ru>
2024-09-13 22:21:49 +0500

    Adjust database settings.

--- back/prostoemenu/settings/base.py
+++ back/prostoemenu/settings/base.py
@@ -93,17 +93,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "prostoemenu.wsgi.application"
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
-#         "NAME": os.getenv("POSTGRES_DB", default="postgres"),
-#         "USER": os.getenv("POSTGRES_USER", default="postgres"),
-#         "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
-#         "HOST": os.getenv("DB_HOST", default="db"),
-#         "PORT": os.getenv("DB_PORT", default="5432"),
-#     }
-# }
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarity"

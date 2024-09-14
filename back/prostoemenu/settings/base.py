cba643324b1d1265940522d2780ac47cb0c3e2eb
asbabushkin <75538563+asbabushkin@users.noreply.github.com>
2024-09-14 16:31:40 +0500

    BACKEND #230 - Settings - Remove sqlite (#231)
    * Remove sqlite.
    
    * Adjust database settings.

--- back/prostoemenu/settings/base.py
+++ back/prostoemenu/settings/base.py
@@ -93,13 +93,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "prostoemenu.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarity"
--- back/prostoemenu/settings/production.py
+++ back/prostoemenu/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *  # noqa: F403
 
-DEBUG = True
+DEBUG = False
 
 DATABASES = {
     "default": {

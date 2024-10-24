90ffb005515702dd6c5aad73e451a85e61b6e4c4
asb <asbabushkin@yandex.ru>
2024-10-24 13:43:24 +0500

    Cleaning code

--- back/users/models.py
+++ back/users/models.py
@@ -1,10 +1,23 @@
 from django.conf import settings as django_settings
-from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+from .validators import UserAgeValidator
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, first_name=None, last_name=None, gender=None, birth_date=None, avatar=None, password=None):
+    def create_user(
+        self,
+        email,
+        username,
+        first_name=None,
+        last_name=None,
+        gender=None,
+        birth_date=None,
+        avatar=None,
+        password=None,
+        is_verified=False
+    ):
         if not email:
             raise ValueError("User must have an email")
 
@@ -15,42 +28,41 @@ class CustomUserManager(BaseUserManager):
             last_name=last_name,
             gender=gender,
             birth_date=birth_date,
-            avatar=avatar)
+            avatar=avatar,
+            is_verified=is_verified
+        )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password=None):
-        user = self.create_user(username, email, password=password)
-        user.is_active = True
-        user.is_staff = True
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
-
 
 class User(AbstractUser):
-
     gender = models.CharField(
         null=True,
-        max_length=1,
+        max_length=django_settings.GENDER_ABBR_LENGTH,
         choices=django_settings.GENDER_CHOICES,
-        default=django_settings.FEMALE_ABBR)
+        default=django_settings.FEMALE_ABBR,
+    )
     birth_date = models.DateField(
-        null=True,
-        verbose_name="Дата рождения")
+        null=True, validators=[UserAgeValidator()], verbose_name="Дата рождения"
+    )
     avatar = models.ImageField(
-        null=True,
-        upload_to="users/avatar",
-        verbose_name="Аватар"
+        null=True, upload_to="users/avatar", verbose_name="Аватар"
     )
     is_verified = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["email", "first_name", "last_name", "birth_date", "gender", "avatar"]
+    EMAIL_FIELD = "email"
+    # Дим, я хз зачем в required_fields нужны имя и фамилия, но без них юзер не создается.
+    # Если можешь объяснить, будет здорово
+    REQUIRED_FIELDS = [
+        "email",
+        "first_name",
+        "last_name",
+    ]
 
     def __str__(self):
         return self.email

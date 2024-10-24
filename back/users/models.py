1e5b418dbb15fc4ba7729c4ee5568cf3c9df03e2
asb <asbabushkin@yandex.ru>
2024-10-24 14:35:58 +0500

    CustomUserManager create_superuser() created.

--- back/users/models.py
+++ back/users/models.py
@@ -36,6 +36,21 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, username, email, first_name, last_name, password):
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        user.set_password(password)
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
 
 class User(AbstractUser):
     gender = models.CharField(

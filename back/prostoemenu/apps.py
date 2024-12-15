b1dfdb9c7170d56752ecf285b143eec647ff47e5
asb <asbabushkin@yandex.ru>
2024-12-15 18:36:56 +0500

    Apply migrations.

---
+++ back/prostoemenu/apps.py
@@ -0,0 +1,9 @@
+# from django.apps import AppConfig
+#
+#
+# class ProstoemenuConfig(AppConfig):
+#     default_auto_field = "django.db.models.BigAutoField"
+#     name = "prostoemenu"
+#
+#     def ready(self):
+#         import prostoemenu.schema  # noqa: E402

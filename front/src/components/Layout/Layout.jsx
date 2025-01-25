d28f0181bcfc2107b16dcf46f1a9e3fd2428826f
sergioivanov008 <sergioivanov008@gmail.com>
2025-01-25 15:46:42 +0300

    fix: fix layout

--- front/src/components/Layout/Layout.jsx
+++ front/src/components/Layout/Layout.jsx
@@ -1,4 +1,3 @@
-import { Outlet } from 'react-router-dom';
 import Modal from 'components/Modal';
 import { ToastNotifications } from 'components/Toast';
 import { Footer, Header } from './elements';
@@ -7,9 +6,6 @@ import styles from './Layout.module.scss';
 const Layout = () => (
   <div className={styles.app}>
     <Header />
-    <main className={styles.main}>
-      <Outlet />
-    </main>
     <Modal />
     <Footer />
     <ToastNotifications />
--- front/src/components/Layout/elements/Header/Header.jsx
+++ front/src/components/Layout/elements/Header/Header.jsx
@@ -1,16 +1,57 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import Navbar from 'components/Navbar';
+import { reset } from 'store/slices/search/searchSlice';
 import Logo from 'ui/Logo';
 import navigation from 'utils/navigation';
 import styles from './Header.module.scss';
 
-const Header = () => (
-  <header className={styles.header}>
-    <Link className={styles.logo_link} to="/">
-      <Logo />
-    </Link>
-    <Navbar navigation={navigation} />
-  </header>
-);
+const Header = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const headerRef = useRef(null);
+  const mainContentRef = useRef(null);
+
+  const handleLogoClick = () => {
+    if (location.pathname === '/') {
+      if (mainContentRef.current) {
+        mainContentRef.current.scrollIntoView({ behavior: 'smooth' });
+      }
+      dispatch(reset());
+    } else {
+      navigate('/');
+    }
+  };
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      const resetAndScroll = () => {
+        dispatch(reset());
+
+        if (mainContentRef.current) {
+          mainContentRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+      };
+      const timeoutId = setTimeout(resetAndScroll, 300);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [location.pathname, dispatch]);
+
+  return (
+    <>
+      <header className={styles.header} ref={headerRef}>
+        <Link className={styles.logo_link} to="/" onClick={handleLogoClick}>
+          <Logo />
+        </Link>
+        <Navbar navigation={navigation} />
+      </header>
+      <main className={styles.main} ref={mainContentRef}>
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
 export default Header;

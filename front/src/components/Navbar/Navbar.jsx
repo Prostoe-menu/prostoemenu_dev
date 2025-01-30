547953ecf565a68b47d56b5ba709d34c4c20f2fb
sergioivanov008 <sergioivanov008@gmail.com>
2025-01-30 13:33:44 +0300

    feat: plug function resetIngredients in to Navbar item 'Главная'

--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,33 @@
+import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import resetIngredients from 'utils/resetIngredients';
 import styles from './Navbar.module.scss';
 
-const Navbar = ({ navigation }) => (
-  <nav>
-    <ul className={styles.list}>
-      {navigation.map((item) => (
-        <li key={item.id} className={styles.item}>
-          <NavLink className={styles.link} to={item.route}>
-            {item.name}
-          </NavLink>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const Navbar = ({ navigation }) => {
+  const dispatch = useDispatch();
+
+  const handleClick = (e) => {
+    if (e.target.dataset.value === 'Главная') resetIngredients(dispatch);
+  };
+
+  return (
+    <nav>
+      <ul className={styles.list}>
+        {navigation.map((item) => (
+          <li key={item.id} className={styles.item}>
+            <NavLink
+              className={styles.link}
+              to={item.route}
+              onClick={handleClick}
+              data-value={item.name}
+            >
+              {item.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;

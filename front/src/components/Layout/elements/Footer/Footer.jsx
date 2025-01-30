74e39aca1d80717f142f94fbb340d75bd7e1b8ee
sergioivanov008 <sergioivanov008@gmail.com>
2025-01-30 13:10:26 +0300

    feat: plug function resetIngredients in to Header and Footer components

--- front/src/components/Layout/elements/Footer/Footer.jsx
+++ front/src/components/Layout/elements/Footer/Footer.jsx
@@ -1,28 +1,38 @@
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Logo from 'ui/Logo';
 import { AGREEMENT_URL } from 'utils/constants';
+import resetIngredients from 'utils/resetIngredients';
 import styles from './Footer.module.scss';
 
-const Footer = () => (
-  <footer className={styles.footer}>
-    <Link className={styles.logo} to="/">
-      <Logo />
-    </Link>
+const Footer = () => {
+  const dispatch = useDispatch();
 
-    <p className={styles.links}>
-      <a
-        className={styles.agreement}
-        href={AGREEMENT_URL}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Пользовательское соглашение
-      </a>
-      <span className={styles.copyright}>
-        &copy;&nbsp;Простое&nbsp;Меню, 2023. Все права защищены.
-      </span>
-    </p>
-  </footer>
-);
+  const handleReset = () => {
+    resetIngredients(dispatch);
+  };
+
+  return (
+    <footer className={styles.footer}>
+      <Link className={styles.logo} to="/" onClick={handleReset}>
+        <Logo />
+      </Link>
+
+      <p className={styles.links}>
+        <a
+          className={styles.agreement}
+          href={AGREEMENT_URL}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Пользовательское соглашение
+        </a>
+        <span className={styles.copyright}>
+          &copy;&nbsp;Простое&nbsp;Меню, 2023. Все права защищены.
+        </span>
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
--- front/src/components/Layout/elements/Header/Header.jsx
+++ front/src/components/Layout/elements/Header/Header.jsx
@@ -1,16 +1,26 @@
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Navbar from 'components/Navbar';
 import Logo from 'ui/Logo';
 import navigation from 'utils/navigation';
+import resetIngredients from 'utils/resetIngredients';
 import styles from './Header.module.scss';
 
-const Header = () => (
-  <header className={styles.header}>
-    <Link className={styles.logo_link} to="/">
-      <Logo />
-    </Link>
-    <Navbar navigation={navigation} />
-  </header>
-);
+const Header = () => {
+  const dispatch = useDispatch();
+
+  const handleReset = () => {
+    resetIngredients(dispatch);
+  };
+
+  return (
+    <header className={styles.header}>
+      <Link className={styles.logo_link} to="/" onClick={handleReset}>
+        <Logo />
+      </Link>
+      <Navbar navigation={navigation} />
+    </header>
+  );
+};
 
 export default Header;

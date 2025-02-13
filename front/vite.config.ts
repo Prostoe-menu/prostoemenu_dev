68d0c6cf53362190787e1bf955c179bd00fd6fcc
azozulya <bbbom@mail.ru>
2025-02-13 15:25:16 +0200

    #278-new-configs

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import path from 'path';
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
+
+import path from 'path';
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint2';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -20,14 +21,11 @@ export default defineConfig({
     open: false,
     port: 3000,
   },
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [react(), tsconfigPaths(), svgr(), eslint()],
   resolve: {
     alias: {
       assets: path.resolve(__dirname, './src/shared/assets'),
       styles: path.resolve(__dirname, './src/shared/styles'),
     },
   },
-  // plugins: [react(), tsconfigPaths(), svgr(), eslint({
-  //   exclude: ['node_modules/**'],
-  // })],
 });
